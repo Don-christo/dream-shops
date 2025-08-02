b0e0e48c605c28c2a132e6aac4412b6c0551efd9
Ikechukwu Odokoro <odokoroikechukwujnr@gmail.com>
2025-08-02 19:57:39 +0100

    first commit for the demo project

---
+++ src/main/java/com/codewithiyke/dreamshops/service/product/IProductService.java
@@ -0,0 +1,19 @@
+package com.codewithiyke.dreamshops.service.product;
+
+import com.codewithiyke.dreamshops.model.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    Product addProduct(Product product);
+    Product getProductById(Long id);
+    void deleteProductById(Long id);
+    void updateProduct(Product product, Long productId);
+    List<Product> getAllProducts();
+    List<Product> getAllProductsByCategory(String category);
+    List<Product> getProductByBrand(String brand);
+    List<Product> getProductByCategoryAndBrand(String category, String brand);
+    List<Product> getProductByName(String name);
+    List<Product> getProductByBrandAndName(String brand, String name);
+    Long countProductsByBrandAndName(String brand,  String name);
+}
---
+++ src/main/java/com/codewithiyke/dreamshops/service/product/ProductService.java
@@ -0,0 +1,4 @@
+package com.codewithiyke.dreamshops.service.product;
+
+public class ProductService {
+}
