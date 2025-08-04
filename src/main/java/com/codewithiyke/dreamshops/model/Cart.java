c8264d677b8fe77af549406e67c78f439941f2fd
Ikechukwu Odokoro <odokoroikechukwujnr@gmail.com>
2025-08-04 20:15:45 +0100

    implement cart and cartItem entities

---
+++ src/main/java/com/codewithiyke/dreamshops/model/Cart.java
@@ -0,0 +1,25 @@
+package com.codewithiyke.dreamshops.model;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Cart {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private BigDecimal totalAmount = BigDecimal.ZERO;
+
+  @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+  private Set<CartItem> cartItems;
+}
