78e404f68d1bc2eb4774bd26757b7d8336fa67ce
Ikechukwu Odokoro <odokoroikechukwujnr@gmail.com>
2025-08-05 02:14:03 +0100

    CartItem service implementation completed

--- src/main/java/com/codewithiyke/dreamshops/service/cart/ICartItemService.java
+++ src/main/java/com/codewithiyke/dreamshops/service/cart/ICartItemService.java
@@ -1,3 +1,13 @@
 package com.codewithiyke.dreamshops.service.cart;
 
-public interface ICartItemService {}
+import com.codewithiyke.dreamshops.model.CartItem;
+
+public interface ICartItemService {
+  void addItemToCart(Long cartId, Long productId, int quantity);
+
+  void removeItemFromCart(Long cartId, Long productId);
+
+  void updateQuantity(Long cartId, Long productId, int quantity);
+
+  CartItem getCartItem(Long cartId, Long productId);
+}
