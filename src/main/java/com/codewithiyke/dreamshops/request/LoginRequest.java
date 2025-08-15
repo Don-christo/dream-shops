ac1bdd118ae0c43b1e523fb8a3cd3f19c09ccb65
Ikechukwu Odokoro <odokoroikechukwujnr@gmail.com>
2025-08-15 01:17:38 +0100

    implemented the login api

---
+++ src/main/java/com/codewithiyke/dreamshops/request/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.codewithiyke.dreamshops.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+  @NotBlank private String email;
+
+  @NotBlank private String password;
+}
